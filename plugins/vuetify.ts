ea1a5a01ce9c212b23968e0de78b724234508759
ZepiaWork <91719737+ZepiaWork@users.noreply.github.com>
2025-07-13 20:36:30 +0700

    Remove unused Vuetify components from plugin setup.

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,27 +2,11 @@
 import "@mdi/font/css/materialdesignicons.css"
 import "vuetify/styles"
 import { createVuetify } from "vuetify"
-import {
-    VApp,
-    VBtn,
-    VCard,
-    VCardText,
-    VCardTitle,
-    VCardSubtitle,
-    VCol,
-    VContainer,
-    VRow,
-    VTextField,
-    VSelect,
-} from "vuetify/components"
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
         // ... your configuration
         ssr: false,
-        components: {
-            VApp,
-        },
     })
     app.vueApp.use(vuetify)
 })
